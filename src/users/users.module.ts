4965a130373ee5150621d4afad04042fad0c230e
Ruslan Hlabay <ruslan.hlabay@gmail.com>
2023-01-30 15:38:23 +0300

    feat(all): add roles controllers

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Role } from 'src/roles/roles.model';
+import { RolesModule } from 'src/roles/roles.module';
 import { UserRoles } from 'src/roles/user-roles.model';
 import { User } from './user.model';
 import { UsersController } from './users.controller';
@@ -10,7 +11,8 @@ import { UsersService } from './users.service';
   controllers: [UsersController],
   providers: [UsersService],
   imports: [
-    SequelizeModule.forFeature([User, Role, UserRoles])
+    SequelizeModule.forFeature([User, Role, UserRoles]),
+    RolesModule
   ]
 })
 export class UsersModule {}
